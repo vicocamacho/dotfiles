4ece3abc3f6a0fb42daeb05d6eab1eb859e85d98
Victor Camacho <victor@codn.mx>
2018-05-29 12:05:59 -0500

    Update prompt info order

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -79,7 +79,7 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status) $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(battery_status) in $(directory_name) $(rb_prompt)$(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
